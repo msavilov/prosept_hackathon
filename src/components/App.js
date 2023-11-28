f6aab1551e64dd1fdcf88eed138c8636054636c5
Kazachuk Nailya <yakimovanailya@yandex.ru>
2023-11-28 13:27:50 +0300

    commit restart

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
+
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
-import Header from './Header';
+//import Header from './Header/Header';
 // import Login from './Login';
 // import Register from './Register';
-import Main from './Main';
-import Footer from './Footer';
+//import Main from './Main';
+//import Footer from './Footer';
 // import Profile from './Profile';
 
-import NotFound from './NotFound';
-import InfoTooltip from './InfoTooltip';
-import Navigation from './Navigation';
-import ProtectedRoute from './ProtectedRoute';
-import * as mainApi from '../utils/MainApi';
-import * as auth from '../utils/auth';
-import Preloader from './Preloader';
+//import NotFound from './NotFound';
+//import InfoTooltip from './InfoTooltip';
+//import Navigation from './Navigation';
+//import ProtectedRoute from './ProtectedRoute';
+//import * as mainApi from '../utils/MainApi';
+//import * as auth from '../utils/auth';
+//import Preloader from './Preloader';
 
 function App() {
   return <h1>Hello world! I am using React</h1>;
