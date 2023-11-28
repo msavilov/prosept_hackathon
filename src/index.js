2b48a2175531a75fb4e31517fa6069685e95ad41
Anna b <anchikfyz@yandex.ru>
2023-11-28 12:02:56 +0500

    feat: skeleton, fonts

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from '../src/components/App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
