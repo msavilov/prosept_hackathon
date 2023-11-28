c6dc68058a460099e214ebcc4ce12c9ea6061d33
Anna b <anchikfyz@yandex.ru>
2023-11-29 01:13:50 +0500

    feature app sceleton, footer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 //import Header from './Header/Header';
-// import Login from './Login';
-// import Register from './Register';
 //import Main from './Main';
-//import Footer from './Footer';
-// import Profile from './Profile';
-
+import Footer from './Footer';
 //import NotFound from './NotFound';
 //import InfoTooltip from './InfoTooltip';
 //import Navigation from './Navigation';
 //import ProtectedRoute from './ProtectedRoute';
-//import * as mainApi from '../utils/MainApi';
-//import * as auth from '../utils/auth';
-//import Preloader from './Preloader';
+//import All-products from './All-products';\
+//import Diler-products from './Diler-products';
+import Preloader from './Preloader';
 
 function App() {
-  return <h1>Hello world! I am using React</h1>;
+  // Стейты
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  return isLoading ? (
+    <Preloader />
+  ) : (
+    <div className='App page'>
+      {/* <Header>
+      <Navigation />
+    </Header> */}
+      <Routes>
+        {/* <Route path='/' element={<Main />} /> */}
+        {/* <Route path='/all-products' element={<All-products />} /> */}
+        {/* <Route path='/diler-products' element={<Diler-products />} /> */}
+        {/* <Route path='/*' element={<NotFound />} /> */}
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
