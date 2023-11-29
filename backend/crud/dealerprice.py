b30ceb438439add6a495fecf2674ae6196654da1
AlexanderPAI <lawlikeart@yandex.ru>
2023-11-29 14:20:26 +0300

    Feat: CRUD-func get_all_dealer_prices

---
+++ backend/crud/dealerprice.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.models.models import DealerPrice
+from backend.schemas.dealerprice import DealerPriceScheme
+
+
+async def get_all_dealer_prices(
+        session: AsyncSession,
+) -> list[DealerPrice]:
+    """Получение из БД списка товаров дилеров."""
+    db_prices_list = await session.execute(
+        select(DealerPrice)
+    )
+    db_prices_list = db_prices_list.scalar().all()
+    return db_prices_list
