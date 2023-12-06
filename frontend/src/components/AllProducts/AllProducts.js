3d8b9dfe8b3d6d4bb6f55ed595b83679bb1cb031
Kazachuk Nailya <yakimovanailya@yandex.ru>
2023-12-06 16:54:37 +0300

    commit products

--- frontend/src/components/AllProducts/AllProducts.js
+++ frontend/src/components/AllProducts/AllProducts.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import products from '../Products/Products';
 import ProductItem from '../ProductItem/ProductItem';
 import Pagination from '../Pagination/Pagination';
 import icon_match from '../../images/icon_match.png';
@@ -8,9 +10,11 @@ function AllProducts(props) {
   const [loading, setLoading] = React.useState(false);
   const [isMatch, setIsMatch] = React.useState(false);
   console.log(isMatch);
+
   const [productsList, setProductsList] = React.useState([]);
   const [views, setViews] = React.useState(10);
   console.log(views);
+  
   const [currentPage] = React.useState(1);
 
   // Match open & close
@@ -21,169 +25,6 @@ function AllProducts(props) {
   function handleMatchClose() {
     setIsMatch(false);
   }
-
-  const products = [
-    {
-      product_key: 546227,
-      product_name: 'Prosept Universal Spray, 300',
-      dealer_id: 2,
-      id: 2,
-      date: '2022-08-03',
-      price: 233.0,
-      product_url: 'https://akson.ru//p/sredstvo_universalnoe_prosept_universal_spray_500ml/',
-      is_marked: false
-    },
-    {
-      product_key: 546408,
-      product_name: 'Prosept Universal Spray, 500',
-      dealer_id: 2,
-      id: 3,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: false
-    },
-    {
-      product_key: 546409,
-      product_name: 'Prosept Universal Spray, 1000',
-      dealer_id: 2,
-      id: 4,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: true
-    },
-    {
-      product_key: 546227,
-      product_name: 'Prosept Universal Spray, 300',
-      dealer_id: 2,
-      id: 5,
-      date: '2022-08-03',
-      price: 233.0,
-      product_url: 'https://akson.ru//p/sredstvo_universalnoe_prosept_universal_spray_500ml/',
-      is_marked: false
-    },
-    {
-      product_key: 546408,
-      product_name: 'Prosept Universal Spray, 500',
-      dealer_id: 2,
-      id: 6,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: false
-    },
-    {
-      product_key: 546409,
-      product_name: 'Prosept Universal Spray, 1000',
-      dealer_id: 2,
-      id: 7,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: true
-    },
-    {
-      product_key: 546227,
-      product_name: 'Prosept Universal Spray, 300',
-      dealer_id: 2,
-      id: 8,
-      date: '2022-08-03',
-      price: 233.0,
-      product_url: 'https://akson.ru//p/sredstvo_universalnoe_prosept_universal_spray_500ml/',
-      is_marked: true
-    },
-    {
-      product_key: 546408,
-      product_name: 'Prosept Universal Spray, 500',
-      dealer_id: 2,
-      id: 9,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: true
-    },
-    {
-      product_key: 546409,
-      product_name: 'Prosept Universal Spray, 1000',
-      dealer_id: 2,
-      id: 9,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: false
-    },
-    {
-      product_key: 546227,
-      product_name: 'Prosept Universal Spray, 300',
-      dealer_id: 2,
-      id: 10,
-      date: '2022-08-03',
-      price: 233.0,
-      product_url: 'https://akson.ru//p/sredstvo_universalnoe_prosept_universal_spray_500ml/',
-      is_marked: true
-    },
-    {
-      product_key: 546408,
-      product_name: 'Prosept Universal Spray, 500',
-      dealer_id: 2,
-      id: 11,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: false
-    },
-    {
-      product_key: 546409,
-      product_name: 'Prosept Universal Spray, 1000',
-      dealer_id: 2,
-      id: 12,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: true
-    },
-    {
-      product_key: 546227,
-      product_name: 'Prosept Universal Spray, 300',
-      dealer_id: 2,
-      id: 13,
-      date: '2022-08-03',
-      price: 233.0,
-      product_url: 'https://akson.ru//p/sredstvo_universalnoe_prosept_universal_spray_500ml/',
-      is_marked: false
-    },
-    {
-      product_key: 546408,
-      product_name: 'Prosept Universal Spray, 500',
-      dealer_id: 2,
-      id: 14,
-      date: '2022-08-03',
-      price: 175.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: true
-    },
-    {
-      product_key: 546409,
-      product_name: 'Prosept Universal Spray, 1000',
-      dealer_id: 2,
-      id: 15,
-      date: '2022-08-03',
-      price: 1705.0,
-      product_url:
-        'https://akson.ru//p/kontsentrat_prosept_multipower_dlya_mytya_polov_tsitrus_1l/',
-      is_marked: true
-    }
-  ];
 
   // value for Views
   function handleViewsChange(e) {
