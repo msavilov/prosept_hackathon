6daa3815477e65f632806da6918a9fb057215810
Anna b <anchikfyz@yandex.ru>
2023-12-08 12:32:14 +0500

    feature: details, buttons

--- frontend/src/components/ProductDetails/ProductDetails.js
+++ frontend/src/components/ProductDetails/ProductDetails.js
@@ -5,6 +5,8 @@ import { marked } from '../../utils/config';
 import Dropdown from '../Dropdown/Dropdown';
 
 function ProductDetails(props) {
+  const [selectedOption, setSelectedOption] = React.useState('');
+
   return (
     <div className='product-details'>
       <h2 className='product-details__title'>{props.selectedProd.product_name}</h2>
@@ -12,14 +14,22 @@ function ProductDetails(props) {
       <p className='text product-details__text'>Price: {props.selectedProd.price}</p>
       <p className='text product-details__text'>Product_url: {props.selectedProd.product_url}</p>
       <h2 className='product-details__title'>Товары для сопоставления</h2>
-      <Dropdown marked={marked} />
-      {/* <table className='products__table'>
-        {marked.map(mark => (
-          <>
-            {mark.name} {mark.article}
-          </>
-        ))}
-      </table> */}
+      <Dropdown
+        marked={marked}
+        selectedOption={selectedOption}
+        setSelectedOption={setSelectedOption}
+      />
+      <div className='product-details__buttons'>
+        <button className='button product-details__button' onClick={() => {}}>
+          Да
+        </button>
+        <button className='button product-details__button' onClick={() => {}}>
+          Нет
+        </button>
+        <button className='button product-details__button' onClick={() => {}}>
+          Отложить
+        </button>
+      </div>
     </div>
   );
 }
